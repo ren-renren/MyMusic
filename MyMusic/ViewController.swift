2b82ba8f5a55a4d9bcad26d0c127eca6be2f64d2
ren-renren <ren.alpineski.1003@gmail.com>
2021-01-10 23:12:47 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by 鈴木蓮 on 2021/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
